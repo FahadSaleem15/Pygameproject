0063fa91b80b1e25e09ca8ed0aafff990f729459
Fahad Saleem <fahad_saleem1@baylor.edu>
2024-10-12 17:47:57 -0500

    increasing point values

--- alien_invasion.py
+++ alien_invasion.py
@@ -100,7 +100,8 @@ class AlienInvasion:
     def _check_bullet_alien_collisions(self):
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
         if collisions:
-              self.stats.score += self.settings.alien_points
+              for aliens in collisions.values():
+                   self.stats.score += self.settings.alien_points * len(aliens)
               self.sb.prep_score()
         if not self.aliens:
               self.bullets.empty()
@@ -146,6 +147,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
             self.aliens.empty()
             self.bullets.empty()
             self._create_fleet()
