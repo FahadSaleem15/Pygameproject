f21a44a3c7a5f82be5b18df87ece86d8a7cfcc09
Fahad Saleem <fahad_saleem1@baylor.edu>
2024-10-12 14:03:08 -0500

    refactoring update bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -62,13 +62,15 @@ class AlienInvasion:
 
     def _update_bullets(self):
         self.bullets.update()
-        if not self.aliens:
-              self.bullets.empty()
-              self._create_fleet()
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                  self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+    def _check_bullet_alien_collisions(self):
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+              self.bullets.empty()
+              self._create_fleet()
         print(len(self.bullets))
     
     def _update_aliens(self):
